1da796299c2ccf5df681ceb3261c472b280b4d97
wasolomon98 <99094288+wasolomon98@users.noreply.github.com>
2022-03-04 18:06:49 -0600

    Update MechBuilder.py

--- MechBuilder.py
+++ MechBuilder.py
@@ -10,7 +10,6 @@ class Pilot:
         self.skills = skills
         self.inventory = inventory
 
-
 class Mech:
     def __init__(self, name, __serial_number='', tier=0):
         self.name = name
@@ -18,18 +17,18 @@ class Mech:
         self.tier = tier
 
 class StartMenu:
-    def titleMenu(self):
+    def launchTitleMenu(self):
         print("Welcome to v0.0.0.1 of the game!")
         print("What would you like to do?:")
         print("(N)ew Game, (C)ontinue, e(X)it)")
         s = str(takeInput())
         try:
             while s:
-                if s.upper() == 'N':
-                    return 0
-                elif s.upper() == 'C':
+                if s.upper() == 'N' or s.upper() == 'NEW GAME':
+                    return 1
+                elif s.upper() == 'C' or s.upper() == 'CONTINUE':
                     pass
-                elif s.upper() == 'X':
+                elif s.upper() == 'X' or s.upper() == 'EXIT':
                     quit()
                 else:
                     print("I didn't understand that.")
@@ -56,24 +55,38 @@ class NewGameHandler:
                     print("How many pilots do you have?:")
                     s = str(self.takeInput())    
 
+
 # Controls the GameManager through a series of automated codes.
 # Makes it easier to jump between different scenes/functions by automating transitions.
 class LogicHandler:
-    # Main 
-    def getAction(self, action_code):
-        pass
+    def getActionCode(self, action_code):
+        __logic_codes = ('title_sequence', 'new_game')
+        while action_code:
+            return __logic_codes[action_code]
+        else:
+            return 'title_sequence'
+
         
-
-
 class GameManager:
+    def __init__(self, code_key={'title_sequence': 0}):
+        self.code_key = code_key
+    
     # Initiates game, handles main menu placeholder
-    def launchTitleMenu(self, start_menu_handler)
+    def launchTitleMenu(self, startMenu=TitleMenu()):
+        startMenu.launchTitleMenu()
 
     def launchNewGame(self, title_menu_output, new_game_handler):
         new_game_handler.newGame(start_game_output)
 
-
+    def runSequence(self, sequence_name):
+        
+        
+__logicHandler = LogicHandler()
 __gameManager = GameManager()
-__startMenu = StartMenu()
-__gameManager.startGame(__startMenu)
-a = __gameManager.startGame()
+a = __gameManager.runSequence(__logicHandler.GetAction(0))
+while a:
+else:
+    if a == 0:
+        a = __gameManager.runSequence(__logicHandler.GetAction(0))
+    else:
+        quit()
